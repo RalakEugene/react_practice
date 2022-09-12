f19c400dc1d973a6b6ed561c753e1314403ac702
Eugene Ralak <ralakeugene@gmail.com>
2022-09-12 12:15:58 +0300

    Using the useState hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 
+import React from 'react';
 import './App.css';
 import Students from './components/Students';
+import { useState} from 'react';
 
 function App() {
+  const [role, setRole] = useState ('dev')
   const showStudents = true;
   return (
     <div className="App">
       {showStudents ? (
         <p>
+          <input type='text' onChange={(e) => {
+            console.log(e.target.value);
+            setRole(e.target.value)
+          }}/>
           <Students name="Gene" role="Front-end developer"/>
-          <Students name="Sid"/>
+          <Students name="Sid" role={role}/>
           <Students name="Sam"/>
         </p>
       ) : (
